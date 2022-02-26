13495d357fcae2ba6e10d273dd10d6e69b79b318
Ganaelle Gilet <gg14j@my.fsu.edu>
2022-02-25 20:27:57 -0500

    added logic for modifying local storage

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ const formContainer = document.getElementsByClassName('.form-container');
 const cardContainer = document.getElementById('cardContainer');
 
 let bookTitle = '';
-let bookAuthor = 'test';
+let bookAuthor = '';
 let bookReleaseYear = '';
 let bookPages = '';
 let isRead = false;
 let userBook;
+let readStatus;
+let buttonIndex = 0;
+let index;
 
 displayCards();
 
@@ -21,6 +24,10 @@ function Book(title, author, releaseYear, pages, isRead) {
 	this.releaseYear = releaseYear;
 	this.pages = pages;
 	this.isRead = isRead;
+}
+
+function setBookToLocalStorage() {
+	localStorage.setItem('Book', JSON.stringify(userLibrary));
 }
 
 function addBookToLibrary() {
@@ -39,8 +46,10 @@ function addBookToLibrary() {
 	);
 
 	userLibrary.push(userBook);
-	localStorage.setItem('Book', JSON.stringify(userLibrary));
+	setBookToLocalStorage();
 }
+
+/* On Click Events */
 
 button.addEventListener('click', () => {
 	addBookToLibrary();
@@ -57,21 +66,92 @@ window.onclick = function (event) {
 	}
 };
 
+let readButtons = document.querySelectorAll('#readButton');
+
+// readButtons.forEach(function (e) {
+// 	e = this;
+// 	console.log(e.target);
+// });
+
+// let index = 0;
+// for (let i = 0; i < userLibrary.length; i++) {
+
+function changeReadStatusAndIndexButtons() {
+	console.log('1');
+	index = readButton.getAttribute('index');
+	if (userLibrary[index].isRead == false) {
+		userLibrary[index].isRead = true;
+		setBookToLocalStorage();
+		console.log(JSON.parse(localStorage.getItem('Book') || '[]'));
+	} else {
+		userLibrary[index].isRead = false;
+		setBookToLocalStorage();
+		console.log(JSON.parse(localStorage.getItem('Book') || '[]'));
+	}
+	// console.log(e.currentTarget);
+	displayCards();
+}
+
+readButton.addEventListener('click', () => {
+	console.log('test');
+});
+
+readButton.addEventListener('click', () => {
+	changeReadStatusAndIndexButtons();
+});
+
+// }
+
+// function toggleReadStatus(e, el) {
+// 	e = e || window.event;
+// 	// console.log(e.target);
+// 	// console.log(this);
+// 	// console.log(el);
+// }
+// readButtons.forEach((readButton) => {
+// 	readButton.addEventListener('click', () => {
+// 		console.log('I clicked');
+// 	});
+// });
+// function displayReadStatus() {
+// 	let readOrNot = document.getElementsByClassName('.readStatus');
+
+// 	for (let i = 0; i < userLibrary.length; i++) {
+// 		readOrNot[i].style.color = 'blue';
+// 	}
+// }
+
+// userLibrary.forEach((readButton) => {
+// 	readButtons.forEach((readButton) => {
+// 		readButton.addEventListener('click', () => {
+// 			console.log(userLibrary.from(readButtons).indexOf(event.target))
+// 		});
+// 	});
+// });
+
 function displayCards() {
 	cardContainer.replaceChildren();
+	userLibrary = JSON.parse(localStorage.getItem('Book') || '[]');
 	for (let i = 0; i < userLibrary.length; i++) {
 		bookAuthor = userLibrary[i].author;
 		bookTitle = userLibrary[i].title;
 		bookReleaseYear = userLibrary[i].releaseYear;
 		bookPages = userLibrary[i].pages;
+		isRead = userLibrary[i].isRead;
+
+		if (isRead == true) {
+			readStatus = 'bookIsRead';
+		} else {
+			readStatus = 'bookIsNotRead';
+		}
 		const cardTemplate = `<div class="library-card">
 									<div class="book-title">
-										<button id="readButton">
-											<span class="material-icons" > done_all </span>
+										<button id="readButton" index="${buttonIndex}">
+											<span onclick=""class="material-icons ${readStatus}" > done_all </span>
 										</button>
 										<h4 title="${bookTitle}">${bookTitle}</h4>
 										<button id="deleteButton">
-											<span class="material-icons"> highlight_off </span>
+											<span class="material-icons "> highlight_off </span>
 										</button>
 									</div>
 									<div class="library-card-content">
@@ -92,6 +172,10 @@ function displayCards() {
 
 		const libraryDiv = document.createElement('div');
 		libraryDiv.innerHTML = cardTemplate;
+
 		cardContainer.appendChild(libraryDiv);
+		buttonIndex++;
 	}
+	buttonIndex = 0;
 }
+// getReadBookStatus();
